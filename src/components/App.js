03deeca3da171e5d61101bc6e668e9c2be7fb5f7
scottrosen14 <scott.rosen14@gmail.com>
2017-12-05 15:26:40 -0800

    created Question component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Question from './Question'
 
-const App = () => (
-  <div>
-    login
-  </div>
-)
+function getInitialState () {
+  return {
+    questions: [{
+      question: "Why don't you understand Redux?",
+      answerChoices: ['You are lame', 'Answer', 'You are lame', 'You are lame']
+    }]
+  }
+}
+
+// function fetchData() {
+//   return fetch('/data')
+//     .then(response => response.json());
+// }
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = getInitialState();
+  }
+
+  componentDidMount () {
+    // fetch data here
+  }
+
+  render () {
+    return (
+      <div className="app">
+        <div>
+          login
+        </div>
+        <Question state= {this.state} questions={this.state.questions} />
+      </div>
+    )
+  }
+}
+
 export default App;
